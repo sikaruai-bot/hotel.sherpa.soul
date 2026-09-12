510e59956ebb39a3956e91629329ccd21cefae5d
Hotel Sherpa Soul <sikaruai@gmail.com>
2026-09-12 19:11:15 +0545

    fix(mail): include all staff Gmail variations (sherpasoul, hotelsherpasoul, hotelsherpasoul2025)

--- api/send-email.js
+++ api/send-email.js
@@ -107,8 +107,9 @@ export default async function handler(req, res) {
 
     const hotelEmail = "info@hotelsherpasoul.com";
     const primaryGmail = "sherpasoul@gmail.com";
+    const altGmail = "hotelsherpasoul@gmail.com";
     const backupGmail = "hotelsherpasoul2025@gmail.com";
-    const staffRecipients = [primaryGmail, hotelEmail, backupGmail];
+    const staffRecipients = [primaryGmail, altGmail, hotelEmail, backupGmail];
     const hotelPhone = "+977-9851068219";
     const hotelAddress = "Thamel Bhagawati Marg 26, Kathmandu, Nepal";
 
